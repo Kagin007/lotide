c055a589acd51976b262a124b31d179633517e13
Adam Schulte <adamschulte148@hotmail.com>
2022-01-17 14:36:09 +0000

    adding tests folder and tests for head.js and assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -6,12 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-//Testing
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual('Adam', "Adam");
-assertEqual('AdAm', 'Adam');
-assertEqual(123, 123);
-assertEqual(12, 14);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,18 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`💖 Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
 const head = function(arr) {
   let head = arr[0]
   return head
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Lighthouse");
-assertEqual(head(["Labs"]), "Labs");
-assertEqual(head([]), undefined);
+module.exports = head;
